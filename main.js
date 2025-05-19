d1f115aee385af19ce09f7ec4b2bc5993967dc55
yrakhwas <yrakhwas@gmail.com>
2025-05-19 17:38:13 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,123 @@
+// function makeSomeP(a,b)
+// {
+//     // for(let i=0; i<10; i++)
+//     // {
+//     //     document.write("<h2>Hello from function</h2>");
+//     // }
+//     return a+b;
+// }
+
+function args()
+{
+    console.log(arguments);
+}
+args(1,2,3,4);
+
+// let array = [1, 2, 3, 4, 5, false, "Hi", 10.44];
+// for (let item of array) {
+//    console.log(item);
+//    //break; continue;
+// }
+// let num = Math.round(Math.random(100)*100);
+// alert(num);
+
+// let sumNim = makeSomeP(10,20);
+// alert(sumNim);
+// let a = 10;
+// while(a>0)
+// {
+//     console.log(a);
+//     a--;
+// }
+
+// for(let i=0; i<10; i++)
+// {
+//     console.log(i);
+// }
+
+// do
+// {
+//     console.log(a);
+//     a--;
+// }
+// while(a>0);
+
+//alert("Hello World");
+// let a = prompt("Enter first number");
+// let b = prompt("Enter second number");
+// console.log(`Sum of ${a} and ${b} is ${+a + +b}`);
+// if(a>b)
+// {
+//     console.log(`First number ${a} is greater than second number ${b}`);
+// }
+// else if(a<b)
+// {
+//     console.log(`Second number ${b} is greater than first number ${a}`);
+// }
+// else
+
+//a>b? console.log(a):console.log(b);
+// while(a!=b)
+// {
+//     if(a>b)
+//     {
+//         console.log(`First number ${a} is greater than second number ${b}`);
+//         a = prompt("Enter first number");
+//     }
+//     else if(a<b)
+//     {
+//         console.log(`Second number ${b} is greater than first number ${a}`);
+//         a = prompt("Enter first number");
+//     }
+// }
+
+// switch(a)
+// {
+//     case '1':
+//         console.log("First number is 1");
+//         break;
+//     case '2':
+//         console.log("First number is 2");
+//         break;
+//     case '3':
+//         console.log("First number is 3");
+//         break;
+//     default:
+//         console.log("First number is not 1, 2 or 3");
+// }
+
+// let answer = confirm("Are you ready to learn JavaScript?");
+// console.log(answer);
+// if(answer)
+// {
+//     alert("Great! Let's start learning.");
+// }
+// console.log("Hello from console");
+// const a = 10.44;
+// let b = "Hello from console";
+// let c = true;
+// let d = null;
+// let e = undefined;
+// let array = [1, 2, 3, 4, 5, false, "Hi", 10.44, [1,2,3]];
+
+// function summ(a,b)
+// {
+//     console.log(`Summ of numbers ${a} + ${b} = ${a+b}`);
+// }
+
+
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(array));
+// console.log(array[3]);
+// console.log(typeof(summ));
+// console.log(2**5);
+
+// console.log(+'3'!==3);
+
+
+//summ(10,33);
